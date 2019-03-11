a5ff10865200d4b3d8abdfdbd874bfef91b7500f
Jean-Luc Aubert <jean-luc.a@web-projet.com>
2019-03-11 13:48:47 +0100

    Ajout du modèle de pages CMS et mise à jour des liens du pied de page

--- src/ContentBundle/Controller/DefaultController.php
+++ src/ContentBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace ContentBundle\Controller;
 
+use MenuBundle\Entity\Categorie;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,8 +10,52 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * Catégorie traitée
+     * @var Categorie
+     */
+    private $category;
     
-
+    /**
+     * @Route("/cms/{slug}", methods={"GET","HEAD"}, name="cms_page")
+     *
+     * @param Request $request
+     *
+     * Récupère les contenus CMS
+     */
+    public function cmsContentAction(Request $request) {
+        $request->setRequestFormat("html");
+        
+        $siteService = $this->container->get('site_service');
+        
+        $routeComponent = $request->get("slug"); // Identifiant de la catégorie concernée
+        
+        if (filter_var($routeComponent, FILTER_VALIDATE_INT)) {
+            $id = (int) $routeComponent;
+            $slug = "";
+        } else {
+            $slug = $routeComponent;
+            $id = 0;
+        }
+        
+        // Récupère la catégorie
+        if ($id !== 0) {
+            // Récupère la catégorie par son id
+            $this->category = $this->getById($id);
+        } else {
+            // Récupère la catégorie par son slug
+            $this->category = $this->getBySlug($slug);
+        }
+        
+        return $this->render(
+            "@Content/cms/cms.html.twig",
+            [
+                "currentCategory" => $this->category,
+                "articles" => $this->getCategoryArticlesCollection(),
+                "phone" => $siteService->getPhoneNumber()
+            ]
+        );
+    }
     
     /**
      * Récupère le slider d'images pour la page d'accueil
@@ -18,5 +63,57 @@ class DefaultController extends Controller
     public function homeSliderAction()
     {
         return $this->render('ContentBundle:Default:index.html.twig');
+    }
+    
+    
+    /**
+     * Récupère la catégorie par l'intermédiaire de son ID
+     * @param int $id
+     * @return Categorie | null
+     */
+    private function getById(int $id) {
+        
+        $category = $this->getDoctrine()
+        ->getManager()
+        ->find(Categorie::class, $id);
+        
+        return $category;
+    }
+    
+    /**
+     * Retourne la catégorie par l'intermédiaire du slug
+     * @param string $slug
+     * @return Categorie|NULL
+     */
+    private function getBySlug(string $slug) {
+        $category = $this->getDoctrine()
+        ->getManager()
+        ->getRepository(Categorie::class)
+        ->findOneBy(["slug" => $slug]);
+        
+        return $category;
+    }
+    
+    /**
+     * Récupère la liste des produits de la catégorie courante
+     * @return array
+     */
+    private function getCategoryArticlesCollection(\MenuBundle\Entity\Categorie $category = null): array {
+        $articles = [];
+        
+        if ($category === null) {
+            $category = $this->category;
+        }
+        
+        $catToArticles = $category->getArticles();
+        
+        foreach ($catToArticles as $catToArticle) {
+            if ($catToArticle->getArticle()->getIsEnabled()) {
+                $articles[] = $catToArticle->getArticle();
+            }
+            
+        }
+        
+        return $articles;
     }
 }
